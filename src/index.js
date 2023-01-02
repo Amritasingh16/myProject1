4863330aeb997e042ab3b5cd7ec0092a3e3a9220
gauravGo7 <grvgoswami2007@gmail.com>
2023-01-02 13:05:27 +0530

    project blog

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express =require('express')
+const bodyParser=require('body-parser');
+
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.set('strictQuery', true)
+mongoose.connect("mongodb+srv://gaurav:Grv20072000@cluster0.3fqqw8s.mongodb.net/gaurav", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
